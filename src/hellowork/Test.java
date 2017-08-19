0a6ab35a666258d02776e111d7773a141622f257
HunterHuang <hunterhuang1470@163.com>
2017-08-19 13:35:41 +0800

    commit

--- src/hellowork/Test.java
+++ src/hellowork/Test.java
@@ -5,5 +5,8 @@ public class Test {
     public static void main(String[] args) {
         String msg = "Hello Law";
         System.out.println(msg);
+
+        System.out.println("This is stash change");
+
     }
 }
